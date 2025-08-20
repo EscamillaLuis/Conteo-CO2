3372d256768452adc5a3607804ee20da7bef40c6
EscamillaLuis <luisescamela132@gmail.com>
2025-08-20 13:28:30 -0600

    Update Obtener_fecha.h

--- source/Obtener_fecha.h
+++ source/Obtener_fecha.h
@@ -3,13 +3,10 @@
 
 #include <stdint.h>
 
-// Inicializa SNTP (cliente NTP)
 void init_sntp_client(void);
 
-// Función que establece la hora (la llama LWIP internamente)
 void set_time_from_sntp(uint32_t sec);
 
-// Tarea que revisa si ha cambiado el día (para reiniciar contadores)
 void tarea_verificar_fecha(void *arg);
 
 #endif // OBTENER_FECHA_H
